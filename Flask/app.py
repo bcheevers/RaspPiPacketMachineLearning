37aadd7ac2a618893406e33da4dac71aa7368d0c
bcheevers <61117120+bcheevers@users.noreply.github.com>
2020-02-16 15:58:53 +0000

    Add files via upload

--- Flask/app.py
+++ Flask/app.py
@@ -1,1 +1,15 @@
+from flask import Flask, send_file
 
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'Hello World'
+@app.route('/return-file')
+def return_file():
+	return send_file("/home/pi/webapp/test.pcap",attachment_filename="test.pcap")
+def file_downloads():
+	return 'pcap downloads'
+
+if __name__ == '__main__':
+	app.run(debug=True,host='0.0.0.0',port=8000)
